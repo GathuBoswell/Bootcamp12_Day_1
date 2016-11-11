9981fed76f77195ce134ab1bb2376d52e1bb70ac
GathuBoswell <gathudev@gmail.com>
2016-11-11 15:46:10 +0300

    added functionality to doctor, nurse and lab_tech classes

--- Hospital_Module_lab.py
+++ Hospital_Module_lab.py
@@ -4,18 +4,34 @@ class Person(object):
         self.__lname = lname
         self.__id_no = id_no
         self._age = age
-
+    def __str__(self):
+        return self.__fname + ' ' + self.__lname
 
 class Doctor(Person):
-    pass
+    def _init__(self,fname, lname, age, id_no, doctor_hospital_id):
+        self.__doctor_hospital_id = doctor_hospital_id
+        super().__init__(self)
+
+    def can_treat_patient(self):
+        return True
 
 
 class Nurse(Person):
-    pass
+    def _init__(self,fname, lname, age, id_no, nurse_hospital_id):
+        self.__nurse_hospital_id = nurse_hospital_id
+        super().__init__(self)
+
+    def can_treat_patient(self):
+        return True
 
 
 class Lab_Tech(Person):
-    pass
+    def _init__(self,fname, lname, age, id_no, lab_tech_hospital_id):
+        self.__lab_tech_id = lab_tech_hospital_id
+        super().__init__(self)
+
+    def can_treat_patient(self):
+        return False
 
 
 class Patient(Person):
