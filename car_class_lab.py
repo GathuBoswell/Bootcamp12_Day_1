7eab2ddd193cd52d46e502006b3202fb2cc72cd7
GathuBoswell <gathudev@gmail.com>
2016-11-11 12:11:13 +0300

    added get_methods for car model and car name

--- car_class_lab.py
+++ car_class_lab.py
@@ -1,5 +1,15 @@
 # car_class_lab.py
 class Car():
-    def __init__(self, car_name):
+    def __init__(self, car_model='GM', car_name='general1'):
         self.__car_name = car_name
+        self.__car_model = car_model
+
+    @property
+    def name(self):
+        return self.__car_name
+
+    @property
+    def model(self):
+        return self.__car_model
+
 
