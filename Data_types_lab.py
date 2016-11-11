c7f30e6871b29f4f6f08d0d65246374fdeb69f02
GathuBoswell <gathudev@gmail.com>
2016-11-11 10:45:10 +0300

    renamed param to test_data

--- Data_types_lab.py
+++ Data_types_lab.py
@@ -1,26 +1,26 @@
 #data_types_lab.py
-def data_type(param):
-    if type(param) == str:
-        return len(param)
-    elif param == None:
+def data_type(test_data):
+    if type(test_data) == str:
+        return len(test_data)
+    elif test_data == None:
         return 'no value'
-    elif type(param) == bool:
-        return param
-    elif type(param) == int:
-        if param < 100:
+    elif type(test_data) == bool:
+        return test_data
+    elif type(test_data) == int:
+        if test_data < 100:
             return 'less than 100'
-        elif param > 100:
+        elif test_data > 100:
             return 'more than 100'
         else:
             return 'equal to 100'
-    elif type(param) == list:
+    elif type(test_data) == list:
         try:
-            if param[2]:
-                return param[2]
+            if test_data[2]:
+                return test_data[2]
         except IndexError:
             return None
-    elif type(param) in [dict, tuple, set]:
-        return (param, ' is of ', (type(param)))
+    elif type(test_data) in [dict, tuple, set]:
+        return (test_data, ' is of ', (type(test_data)))
     return 'Unrecognized Data type'
 
 print(data_type([1,2]))
