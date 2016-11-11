fd80cb495ad83ffecbeddcf7d134af6ee1fecb41
GathuBoswell <gathudev@gmail.com>
2016-11-11 13:59:06 +0300

    improved on error checking

--- car_class_lab.py
+++ car_class_lab.py
@@ -1,12 +1,12 @@
 # car_class_lab.py
 class Car():
-    def __init__(self, car_model='GM', car_name='general1', car_doors=4, car_wheels=4, car_type='saloon'):
+    def __init__(self, car_name='GM', car_model='general1', car_type='saloon', car_doors=4, car_wheels=4):
         self.__car_name = car_name
         self.__car_model = car_model
         self._car_doors = car_doors
         self.__car_type = car_type
         self._car_wheels = car_wheels
-        self._speed = 0
+        self.speed = 0
 
     @property
     def name(self):
@@ -21,6 +21,7 @@ class Car():
         if self.__car_model in ['Koenigsegg', 'Porshe']:
             self._car_doors = 2
             return self._car_doors
+        return self._car_doors
 
     @property
     def num_of_wheels(self):
@@ -29,7 +30,6 @@ class Car():
             return self._car_wheels
         return self._car_wheels
 
-    @property
     def is_saloon(self):
         if self.__car_type == 'saloon':
             return True
@@ -43,14 +43,44 @@ class Car():
         return self._car_wheels
 
     @property
-    def speed(self):
-        return self._speed
+    def type(self):
+        return self.__car_type
 
-    def drive(self, drive_number):
-        if self.__car_type == 'trailer':
+    def drive(self,  drive_number):
+        if self.type == 'trailer':
             if drive_number >= 1:
-                self._speed = 77
+                self.speed = 77
+                return self
+            return self
         else:
             if drive_number >= 1:
-                self._speed = 1000
+                self.speed = 1000
+                return self
+            return self
+def main():
+    man = Car('MAN', 'Truck', 'trailer')
+    moving_man = man.drive(7)
+    moving_man_instance = isinstance(moving_man, Car)
+    moving_man_type = type(moving_man) is Car
+    print(moving_man_instance, moving_man_type, moving_man.speed)
 
+    man = Car('Mercedes', 'SLR500')
+    parked_speed = man.speed
+    moving_speed = man.drive(3).speed
+
+    print(parked_speed, moving_speed)
+
+    honda = Car('Honda')
+    print(type(honda) is Car)
+
+    man = Car('MAN', 'Truck', 'trailer')
+    koenigsegg = Car('Koenigsegg', 'Agera R')
+    print(man.num_of_wheels, koenigsegg.num_of_wheels)
+
+    opel = Car('Opel', 'Omega 3')
+    porshe = Car('Porshe', '911 Turbo')
+    print(opel.num_of_doors,porshe.num_of_doors,Car('Koenigsegg', 'Agera R').num_of_doors)
+
+    toyota = Car('Toyota', 'Corolla')
+    print(toyota.name, toyota.model)
+if __name__ == '__main__':main()
